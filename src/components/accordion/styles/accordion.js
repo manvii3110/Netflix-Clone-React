1b4aab2d6c7ddc92504e6d98520f7ab196353699
Manvi <msinghal3102@gmail.com>
2021-08-07 15:38:21 +0530

    Features and header are added

--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -1,15 +1,15 @@
 import styled from 'styled-components/macro';
 
-export const Container = styled.div`
+export const Container = styled.div `
   display: flex;
   border-bottom: 8px solid #222;
 `;
 
-export const Frame = styled.div`
+export const Frame = styled.div `
   margin-bottom: 40px;
 `;
 
-export const Inner = styled.div`
+export const Inner = styled.div `
   display: flex;
   padding: 70px 45px;
   flex-direction: column;
@@ -17,7 +17,7 @@ export const Inner = styled.div`
   margin: auto;
 `;
 
-export const Title = styled.h1`
+export const Title = styled.h1 `
   font-size: 50px;
   line-height: 1.1;
   margin-top: 0;
@@ -29,7 +29,7 @@ export const Title = styled.h1`
   }
 `;
 
-export const Item = styled.div`
+export const Item = styled.div `
   color: white;
   margin: auto;
   margin-bottom: 10px;
@@ -43,7 +43,7 @@ export const Item = styled.div`
   }
 `;
 
-export const Header = styled.div`
+export const Header = styled.div `
   display: flex;
   justify-content: space-between;
   cursor: pointer;
@@ -67,7 +67,7 @@ export const Header = styled.div`
   }
 `;
 
-export const Body = styled.div`
+export const Body = styled.div `
   font-size: 26px;
   font-weight: normal;
   line-height: normal;
