46f01e53ee8c19234f31fb0b8e7072c0b91b64c2
Manvi <msinghal3102@gmail.com>
2021-08-10 18:56:40 +0530

    Profile is added and browser page is edited

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link as ReactRouterLink } from 'react-router-dom';
-import { Background, Container, ButtonLink, Logo } from './styles/header';
+import { Background, Container, ButtonLink, Logo, Feature, Text, FeatureCallOut, PlayButton, Link } from './styles/header';
 
 export default function Header({ bg = true, children, ...restProps}){
     return bg? <Background {...restProps}>{children}</Background> : children;
@@ -10,10 +10,26 @@ Header.Frame = function HeaderFrame({children,...restProps}){
     return <Container {...restProps}>{children}</Container>;
 };
 
+Header.Feature = function HeaderFeature({children,...restProps}){
+    return <Feature {...restProps}>{children}</Feature>;
+};
+Header.FeatureCallOut = function HeaderFeatureCallOut({children,...restProps}){
+    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.Text= function HeaderText({children,...restProps}){
+    return <Text {...restProps}>{children}</Text>;
+};
 Header.ButtonLink = function HeaderButtonLink({children,...restProps}){
     return <ButtonLink {...restProps}>{children}</ButtonLink>;
 };
 
 Header.Logo = function HeaderLogo({to,...restProps}){
     return <ReactRouterLink to={to}><Logo {...restProps} /></ReactRouterLink>;
+};
+Header.PlayButton= function HeaderPlayButton({children,...restProps}){
+    return <PlayButton {...restProps}>{children}</PlayButton>;
+};
+Header.TextLink= function HeaderTextLink({children,...restProps}){
+    return <Link {...restProps}>{children}</Link>;
 };
--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -6,6 +6,9 @@ export const Background = styled.div`
   flex-direction: column;
   background: linear-gradient(to bottom, rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.35)), url(${({ src }) => (src ? `../images/misc/${src}.jpg` : '../images/misc/home-bg.jpg')}) top left / cover
   no-repeat;
+  @media (max-width: 1100px) {
+    ${({ dontShowOnSmallViewPort }) => dontShowOnSmallViewPort && `background: none;`}
+  }
 `;
 
 export const Container = styled.div`
@@ -51,5 +54,60 @@ export const ButtonLink = styled(ReactRouterLink)`
 
   &:hover {
     background: #f40612;
+  }
+`;
+
+export const Feature = styled(Container)`
+  padding: 150px 0 500px 0;
+  flex-direction: column;
+  align-items: normal;
+  width: 50%;
+  @media (max-width: 1100px) {
+    display: none;
+  }
+`;
+export const Text = styled.p`
+  color: white;
+  font-size: 22px;
+  line-height: normal;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+`;
+export const FeatureCallOut = styled.h2`
+  color: white;
+  font-size: 50px;
+  line-height: normal;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+  margin: 0;
+`;
+export const PlayButton = styled.button`
+  box-shadow: 0 0.6vw 1vw -0.4vw rgba(0, 0, 0, 0.35);
+  background-color: #e6e6e6;
+  color: #000;
+  border-width: 0;
+  padding: 10px 20px;
+  border-radius: 5px;
+  max-width: 130px;
+  font-weight: bold;
+  font-size: 20px;
+  margin-top: 10px;
+  cursor: pointer;
+  transition: background-color 0.5s ease;
+  &:hover {
+    background-color: #ff1e1e;
+    color: white;
+  }
+`;
+export const Link = styled.p`
+  color: #fff;
+  text-decoration: none;
+  margin-right: 30px;
+  font-weight: ${({ active }) => (active === 'true' ? '700' : 'normal')};
+  cursor: pointer;
+  &:hover {
+    font-weight: bold;
+  }
+  &:last-of-type {
+    margin-right: 0;
   }
 `;
